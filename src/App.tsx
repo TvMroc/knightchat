4ada70e70f63e38a90f73e944827ea6bdc8709e6
TvMroc <1207837@student.roc-nijmegen.nl>
2025-05-08 14:14:21 +0200

    backimg test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import image from './assets/image.png'
 import './App.css'
 
 function App() {
@@ -24,8 +25,19 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <div>
-        <img />
+      <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
+        <img src={image} style={{height: '200px', padding: 0, margin: 0}}/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
