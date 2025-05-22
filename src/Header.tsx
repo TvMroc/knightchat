5d739fd9d0e9ce7fb70f3ae220fef801efd21365
Yiming-Cao <1192702@student.roc-nijmegen.nl>
2025-05-22 13:57:16 +0200

    header upgrade

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,13 +8,15 @@ const Header: React.FC = () => (
         <h2>KnightChat</h2>
     </div>
     <div className="menu">
-        <a href="#">Home</a>
-        <a href="#">Chat</a>
-        <a href="#">Post</a>
-        <a href="#">Profile</a>
+        <a href="#"><h3>Home</h3></a>
+        <a href="#"><h3>Chat</h3></a>
+        <a href="#"><h3>Post</h3></a>
+        <a href="#"><h3>Profile</h3></a>
     </div>
     <div className="login">
-        <button className="login-button">Login</button>
+        <button className="login-button">
+          <img src="./assets/login.png" alt="" />
+        </button>
     </div>
   </header>
 );
