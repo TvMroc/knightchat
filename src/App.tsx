d68f3b50376c55854750a424acd230fb449b6d7c
TvMroc <1207837@student.roc-nijmegen.nl>
2025-05-28 16:22:03 +0200

    delete
    refresh required before deleting new msg

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Delete from './assets/delete.png'
 import './App.css'
 import Header from './Header'
 import Login from './login';
 import Register from './register';
 import { db } from './Firebase';
 import Chat from './chat';
-import { collection, deleteDoc, doc, getDocs, setDoc, type DocumentData } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, QuerySnapshot, setDoc, type DocumentData } from 'firebase/firestore';
 
 function App() {
   const [count, setCount] = useState<number>(0);
-  const [data, setData] = useState<DocumentData[]>([]);
+  const [data, setData] = useState<QuerySnapshot<DocumentData, DocumentData>>();
   const [message, setMessage] = useState<string>('');
   const messagesRef = collection(db, "messages");
 
@@ -32,10 +33,9 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const querySnapshot = await getDocs(messagesRef);
-      console.log('Fetched data:', querySnapshot.docs);
-      const data = querySnapshot.docs.map(doc => doc.data());
+      const data = await getDocs(messagesRef);
       setData(data);
+      console.log(data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -64,9 +64,11 @@ function App() {
                 count is {count}
               </button>
               <div>
-                {data.map((doc: DocumentData, id: number) => (
-                  <p key={id}>{doc.message} <button onClick={() => deleteMessage(id)}>a</button></p>
-                ))}
+                {data?.docs.map((doc) => {
+                  const info = doc.data();
+                  return(
+                  <p>{info.message} <button className='button' onClick={() => deleteMessage(doc._document.key.path.segments[6])}><img width={40} src={Delete}/></button></p>
+                )})}
               </div>
               <input
                 type="text"
